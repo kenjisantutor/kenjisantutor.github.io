dd3a69fdcbbf891b71e32a9b83ba0606fb94ae67
kenjisantutor <kenjisantutor@gmail.com>
2026-02-25 10:55:15 +0800

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const galleryImages = document.querySelectorAll('.gallery img');
-const largeImage = document.getElementById('largeImage');
-
-galleryImages.forEach(img => {
-    img.addEventListener('click', () => {
-        largeImage.src = img.src;
-    });
-});
